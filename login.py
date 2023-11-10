102d2b284af0a4dfdc48f5e8c279ec5ed1e69f80
Pablo Zamora <42967124+pablozmr@users.noreply.github.com>
2023-11-10 10:56:36 -0300

    Update login.py

--- login.py
+++ login.py
@@ -27,13 +27,13 @@ def usuario():
         editar_page()
         
 
-    df = pd.read_csv('placas.csv')
+    df = pd.read_csv("placas.csv")
     st.subheader('Tabla de stocks')
     st.dataframe(df)
     
     
 def buscar_page():
-    df = pd.read_csv('placas.csv')
+    df = pd.read_csv("placas.csv")
     parametro = st.selectbox('Seleccione por cual parametro desea buscar', ("Fuente", "Televisor", "Numero"))
     if parametro == "Fuente":
         fuente = st.text_input("")
@@ -52,14 +52,14 @@ def buscar_page():
             st.dataframe(df.loc[df['nro'] == nro])
 
 def editar_page():
-    df = pd.read_csv('placas.csv')
+    df = pd.read_csv("placas.csv")
     st.column_config.TextColumn("fuente")
     edited_df = st.data_editor(df, num_rows="dynamic")
     
     df = edited_df
     
     if st.button("Editar"):
-        df.to_csv('placas.csv', index= False)
+        df.to_csv("placas.csv", index= False)
         
 
 def main():
