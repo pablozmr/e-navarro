5c58032e8f324f5197f33faca17a59f49037a0bc
Pablo Zamora <42967124+pablozmr@users.noreply.github.com>
2023-11-14 06:27:46 -0300

    Add files via upload

--- prueba.py
+++ prueba.py
@@ -5,7 +5,12 @@ from streamlit_gsheets import GSheetsConnection
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
 
-df = conn.read()
+df = conn.read(
+    worksheet="pet",
+    ttl="10m",
+    usecols=[0, 1],
+    nrows=3,
+)
 
 for row in df.itertuples():
     st.write(f"{row.name} has a :{row.pet}:")
